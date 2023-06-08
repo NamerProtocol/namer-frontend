f53ac9ee374ff2583764395db9d709fbe39396b8
bartenev.ta <bartenev.ta@esphere.ru>
2023-06-08 22:08:46 +0300

    some

--- src/pages/OwnedDomains/components/OwnedDomainItem/OwnedDomainItem.tsx
+++ src/pages/OwnedDomains/components/OwnedDomainItem/OwnedDomainItem.tsx
@@ -27,14 +27,14 @@ export const OwnedDomainItem: FC<Domain> = ({
         <div className={CnOwnedDomainItem()}>
             <div className={CnOwnedDomainItem('left')}>
                 <div className={CnOwnedDomainItem('name')}>{fullName}</div>
-                <div className={CnOwnedDomainItem('expire')}>
+                {/* <div className={CnOwnedDomainItem('expire')}>
                     <div className={CnOwnedDomainItem('expire-label')}>
                         Expires on
                     </div>
                     <div className={CnOwnedDomainItem('expire-date')}>
                         {moment().format('YYYY/MM/DD HH:mm')}
                     </div>
-                </div>
+                </div> */}
             </div>
             <div className={CnOwnedDomainItem('right')}>
                 <div className={CnOwnedDomainItem('price')}>
